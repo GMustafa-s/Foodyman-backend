4e493850ced76079cb29b6d133d6abc2d40af034
Ghulam Mustafa <mustafa@saivra.co>
2025-03-07 02:21:51 -0500

    Initial commit

---
+++ config/credential.php
@@ -0,0 +1,8 @@
+<?php
+ return [
+                        
+'purchase_id' => '23235656',
+                            
+'purchase_code' => '5665653236656',
+                        
+];
---
+++ config/init.php
@@ -0,0 +1,14 @@
+<?php
+ return [
+                        
+'name' => 'Foodyman.pk',
+                        
+'favicon' => 'https://example/storage/images/shops/0001-1741331804.webp',
+                        
+'logo' => 'https://example/storage/images/shops/0001-1741331798.webp',
+                        
+'delivery' => '1',
+                        
+'shop_type' => '1',
+                        
+];
